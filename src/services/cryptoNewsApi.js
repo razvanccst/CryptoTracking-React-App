34fb051cda239dd5feb8c8d4a627ec1421333d06
razvanccst <razvanccst@gmail.com>
2022-09-20 17:20:32 +0300

    Exchanges Not working and Individual Crypto Details

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '9a190e9561msha050e074c161cd8p156a63jsn9b984d8ca0f9',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/';
+
+const createRequest = (url) => ({url, headers:cryptoNewsHeaders})
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+          query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),
+      })
+})
+
+export const {useGetCryptoNewsQuery} = cryptoNewsApi;
